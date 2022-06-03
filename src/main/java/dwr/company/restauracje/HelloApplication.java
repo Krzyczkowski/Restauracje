13e55ed88ccc5e9d12d8fc457d67bce5edcbb964
rrnax <93451852+rrnax@users.noreply.github.com>
2022-06-03 09:27:05 +0200

    Nax done it

--- src/main/java/dwr/company/restauracje/HelloApplication.java
+++ src/main/java/dwr/company/restauracje/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 500);
-        stage.setTitle("aplikacja dziala!");
+        stage.setTitle("aplikacja dziala od Rafała!");
         stage.setScene(scene);
         stage.show();
     }
