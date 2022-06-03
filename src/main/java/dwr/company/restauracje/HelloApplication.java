11f4f2d5caff71fdf9562e757b4bf4d11d1396fc
rrnax <93451852+rrnax@users.noreply.github.com>
2022-06-03 15:11:45 +0200

    LogPanel02

--- src/main/java/dwr/company/restauracje/HelloApplication.java
+++ src/main/java/dwr/company/restauracje/HelloApplication.java
@@ -1,19 +1,42 @@
 package dwr.company.restauracje;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private double xCordinates = 0;
+    private double yCordinates = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logPanel.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.initStyle(StageStyle.UNDECORATED);
+
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                xCordinates = mouseEvent.getSceneX();
+                yCordinates = mouseEvent.getSceneY();
+            }
+        });
+
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                stage.setX(mouseEvent.getSceneX() - xCordinates);
+                stage.setY(mouseEvent.getSceneY() - yCordinates);
+            }
+        });
+
+
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
