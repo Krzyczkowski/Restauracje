3f3dec0a042df242b8808dc8fcaba074f2c81008
K4am <krzyczkowski.wiktor00@gmail.com>
2022-06-04 12:23:28 +0200

    main update (without button controller)

--- src/main/java/dwr/company/restauracje/HelloApplication.java
+++ src/main/java/dwr/company/restauracje/HelloApplication.java
@@ -16,9 +16,9 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logPanel.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.initStyle(StageStyle.UNDECORATED);
+        //stage.initStyle(StageStyle.UNDECORATED);
 
         scene.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
