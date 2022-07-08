74b8037cfddd8ecba8454807e1334a4c5b43fda5
K4am <krzyczkowski.wiktor00@gmail.com>
2022-07-08 13:27:45 +0200

    entity manager

--- src/main/java/dwr/company/restauracje/DatabaseAPI.java
+++ src/main/java/dwr/company/restauracje/DatabaseAPI.java
@@ -1,8 +1,15 @@
 package dwr.company.restauracje;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.sql.*;
 public class DatabaseAPI {
     //polaczenie z PSQL
     private void connect(){
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "jpa" );
+
+        EntityManager entitymanager = emfactory.createEntityManager( );
+        entitymanager.getTransaction().begin( );
 
     }
 
