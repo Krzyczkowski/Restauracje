79707b4e0f0c451b2d9a3ba92c75853f21616700
DarekCich <darek.c65@gmail.com>
2022-07-27 22:48:30 +0200

    app v1.0 start

--- src/main/java/dwr/company/restauracje/GeneralController.java
+++ src/main/java/dwr/company/restauracje/GeneralController.java
@@ -411,8 +411,6 @@ public class GeneralController{
     }
     @FXML
     public void loadProductToTable() throws Exception {
-        choseCategory.getItems().clear();
-        choseCategory.getItems().addAll(Client.getCategories());
         productName.setCellValueFactory(new PropertyValueFactory<Products, String>("name"));
         productCategory.setCellValueFactory(new PropertyValueFactory<Products, String>("category"));
         productPrice.setCellValueFactory(new PropertyValueFactory<Products, Float>("price"));
@@ -427,6 +425,8 @@ public class GeneralController{
         else
             productList.addAll(Client.getProducts());
         tableWithProducts.setItems(productList);
+        choseCategory.getItems().clear();
+        choseCategory.getItems().addAll(Client.getCategories());
     }
 
     @FXML
